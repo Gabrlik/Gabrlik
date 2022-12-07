cd79dd4c85ee5ac158ff1ce3e6e7bda051f1a551
Gabrlik <118371577+Gabrlik@users.noreply.github.com>
2022-12-07 08:44:18 +0100

    Add files via upload

---
+++ 3-1 Names.py
@@ -0,0 +1,18 @@
+# Names - test1
+def my_function(my_list):
+  for item in my_list:
+    print(item)
+my_list = ["Lukas1", "Petr1", "Jindra1", "Jose1","Adam1"]
+my_function(my_list)
+
+# Names - test2
+my_freinds = ["Lukas2", "Petr2", "Jindra2", "Jose2","Adam2"]
+print(my_freinds)
+
+# Names - test3
+my_name = ['Lukas3', 'Petr3', 'Jindra3', 'Jose3',"Adam3"]
+print (my_name.pop())
+print(my_name.pop())
+print(my_name.pop())
+print(my_name.pop())
+print(my_name.pop())
